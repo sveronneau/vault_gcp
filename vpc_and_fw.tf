66fce5ad506124641b7cf5d5979701bf9efa6d0f
Stacy Véronneau <15238253+sveronneau@users.noreply.github.com>
2020-04-27 14:02:36 -0400

    Update vpc_and_fw.tf

--- vpc_and_fw.tf
+++ vpc_and_fw.tf
@@ -1,20 +1,20 @@
-resource "google_compute_network" "default" {
+resource "google_compute_network" "vault-custom-vpc" {
   name                    = "vault-vpc"
   description             = "Vault VPC"
   auto_create_subnetworks = "false"
   project                 = var.project
 }
 
-resource "google_compute_subnetwork" "default" {
+resource "google_compute_subnetwork" "vault-vpc-subnetwork" {
   name          = "vault-subnet-nane1"
   description   = "Vault Subnet"
   ip_cidr_range = "10.0.0.0/16"
-  network       = "google_compute_network.default.self_link"
+  network       = "google_compute_network.vault-custom-vpc.self_link"
   region        = "northamerica-northeast1"
   project       = var.project
 }
 
-resource "google_compute_firewall" "default" {
+resource "google_compute_firewall" "vault-vpc-firewall" {
   name    = "vault-vpc-firewall"
   network = "vault-vpc"
   project = var.project
