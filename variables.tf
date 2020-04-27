550ee692e28d438380f9c4aec72459c16f45ab98
Stacy Véronneau <15238253+sveronneau@users.noreply.github.com>
2020-04-27 11:07:55 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "project" {
+  default = "your_project_id" 
+}
+
+variable "region" {
+  default = "northamerica-northeast1" 
+}
+
+variable "zone" {
+  default = "northamerica-northeast1-a" 
+}
