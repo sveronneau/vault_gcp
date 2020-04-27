484f2b4da61f70f3b88825795e0e0e80a2956542
Stacy Véronneau <15238253+sveronneau@users.noreply.github.com>
2020-04-27 09:50:11 -0400

    Delete variables.tf

--- variables.tf
+++
@@ -1,4 +0,0 @@
-variable "credentials" { default = "your_account.json" }
-variable "project"     { default = "your_project_id" }
-variable "region"      { default = "northamerica-northeast1" }
-variable "zone"        { default = "northamerica-northeast1-a" }
