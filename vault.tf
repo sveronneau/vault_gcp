e91f88bc921e719013231480ab56ee0704ba499c
Stacy Véronneau <15238253+sveronneau@users.noreply.github.com>
2020-04-27 09:58:02 -0400

    Update vault.tf

--- vault.tf
+++ vault.tf
@@ -1,7 +1,7 @@
 provider "google" {
-  credentials = "${var.credentials}"
-  project = "${var.project}"
-  region  = "${var.region}"
+  credentials = ${var.credentials}
+  project = ${var.project}
+  region  = ${var.region}
 }
 
 resource "google_compute_instance" "ansible" {
@@ -20,7 +20,7 @@ resource "google_compute_instance" "ansible" {
 # This is where we configure the instance with ansible-playbook
   provisioner "local-exec" {
     command = "sleep 90; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u your_sshkey_user --private-key your_private_key -i '${google_compute_instance.ansible.network_interface.0.access_config.0.assigned_nat_ip}', master.yml"  }
-
+  
   network_interface {
     network       = "default"
 
