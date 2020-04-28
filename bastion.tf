0ace62fb89fee24b90dee73836b5f58f2ba058de
Stacy Véronneau <15238253+sveronneau@users.noreply.github.com>
2020-04-28 10:03:00 -0400

    Create bastion.tf

---
+++ bastion.tf
@@ -0,0 +1,37 @@
+resource "google_compute_instance" "default" {
+  project      = var.project
+  zone         = var.zone
+  name         = "vault-bastion"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-lts"
+    }
+  }
+
+# This is where we configure the instance with ansible-playbook
+  #provisioner "local-exec" {
+  #  command = "sleep 90; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u your_sshkey_user --private-key your_private_key -i '${google_compute_instance.ansible.network_interface.0.access_config.0.assigned_nat_ip}', master.yml"
+  #  }
+
+  network_interface {
+    network       = "default"
+
+    #access_config { //commenting this section will remove private IP assignment
+    #  // Ephemeral IP
+    #}
+  }
+
+  network_interface {
+    subnetwork    = "vault-subnet-nane1"
+
+    #access_config { //commenting this section will remove private IP assignment
+    #  // Ephemeral IP
+    #}
+  }
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
