fac42c87c30382dfafa0cca1f472567c55dd4774
Stacy Véronneau <15238253+sveronneau@users.noreply.github.com>
2020-05-22 13:20:31 -0400

    Create startup-script.sh

---
+++ startup-script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if sudo blkid /dev/sdb;then
+    exit
+else
+    sudo mkfs.ext4 -m 0 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdb; \
+    sudo mkdir -p /opt/vault/data
+    sudo mount -o discard,defaults /dev/sdb /opt/vault/data
+    echo UUID=`sudo blkid -s UUID -o value /dev/sdb` /opt/vault/data ext4 discard,defaults,nofail 0 2 | sudo tee -a /etc/fstab
+    sudo chown -R vault:vault /opt/vault/data
+    sudo chmod -R 777 /opt/vault/data
+    sudo sed -i "s/_HOSTNAME/$HOSTNAME/g" /opt/vault/config/vault-node-config.hcl
+fi
