8362cd6d20bb1d6eced753788a80387fe70092c6
Stacy Véronneau <15238253+sveronneau@users.noreply.github.com>
2020-05-22 12:31:13 -0400

    Update vault.tf

--- vault.tf
+++ vault.tf
@@ -11,6 +11,12 @@ resource "google_compute_instance" "vault" {
     }
   }
 
+resource "google_compute_disk" "vault" {
+  name  = "vault-data-${count.index}"
+  type  = "pd-ssd"
+  zone  = var.zone
+}
+  
 # This is where we configure the instance with ansible-playbook
   #provisioner "local-exec" {
   #  command = "sleep 90; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u your_sshkey_user --private-key your_private_key -i '${google_compute_instance.ansible.network_interface.0.access_config.0.assigned_nat_ip}', master.yml"
