a50233ef967d98868569abb8731a7103a0b92340
Stacy Véronneau <15238253+sveronneau@users.noreply.github.com>
2020-05-25 14:24:17 -0400

    Update startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -8,5 +8,7 @@ else
     echo UUID=`sudo blkid -s UUID -o value /dev/sdb` /opt/vault/data ext4 discard,defaults,nofail 0 2 | sudo tee -a /etc/fstab
     sudo chown -R vault:vault /opt/vault/data
     sudo chmod -R 777 /opt/vault/data
+    sudo chown -R vault:vault /opt/vault/log
+    sudo chmod -R 777 /opt/vault/log
     sudo sed -i "s/_HOSTNAME/$HOSTNAME/g" /opt/vault/config/vault-node-config.hcl
 fi
